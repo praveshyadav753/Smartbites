372926824d9bd2d8d51b0d98a059e51e9d49c17f
praveshyadav753 <praveshyadav9111@gmail.com>
2024-12-24 01:49:33 +0530

    logout

--- Frontend/src/Components/loginVerify/Login.jsx
+++ Frontend/src/Components/loginVerify/Login.jsx
@@ -67,11 +67,11 @@ const Login = () => {
       });
 
       console.log(response.data.message);
-      const csrfToken = response.headers['x-csrftoken']; // Assuming the CSRF token is sent in this header
-      console.log(csrfToken);
+      const csrfToken = response.headers['x-csrftoken'];
+            console.log(csrfToken);
       if (csrfToken) {
         // Store CSRF token in a cookie for future requests
-        Cookies.set('csrftoken', 'your-csrf-token-value', { path: '/', secure: true, sameSite: 'Strict' });
+        Cookies.set('csrftoken', csrfToken, { path: '/', secure: true, sameSite: 'Strict' });
       }
       // Check if the response contains success
       if (response.status === 200) {
